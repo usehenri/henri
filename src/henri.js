f1a69c33284aa52ad42f032bc7ff9b1a88bfda73
Félix-Antoine Paradis <felix@ozyx.net>
2017-02-09 22:12:30 -0500

    refactoring henri server file

--- src/henri.js
+++ src/henri.js
@@ -1,18 +1,20 @@
 'use strict';
 
+const debug = require('debug')('henri:main');
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const compress = require('compression');
 const cors = require('cors');
-const serveStatic = require('feathers').static;
+
 const feathers = require('feathers');
+const serveStatic = require('feathers').static;
 const auth = require('feathers-authentication');
 const local = require('feathers-authentication-local');
 const jwt = require('feathers-authentication-jwt');
 const hooks = require('feathers-hooks');
 const rest = require('feathers-rest');
-const socketio = require('feathers-socketio');
-const next = require('next');
+
 const config = require('./config');
 const cookies = require('./cookies');
 const catcher = require('./catcher');
@@ -21,67 +23,103 @@ const app = feathers();
 
 app.configure(config);
 
-const view = next({
-  dir: app.get('next'),
-  dev: true,
-  quiet: true
-});
+const isProduction = app.get('env') === 'production';
 
-exports.init = function () {
+const init = () => {
+  // Basic configuration
+  app.use(compress());
+  app.options('*', cors());
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.configure(hooks());
+  app.configure(rest());
+
   if (app.get('public')) {
+    debug('serving static files from', app.get('public'));
     app.use('/', serveStatic(app.get('public')));
   }
-  app.use(compress())
-    .options('*', cors())
-    .use(cors())
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: true }))
-    .use(cookieParser())
-    .configure(hooks())
-    .configure(rest());
 
   if (app.get('socketio')) {
-    console.log('starting socketio');
+    debug('socket.io enabled.');
+    const socketio = require('feathers-socketio');
     app.configure(socketio());
   }
+
+  if (app.get('auth')) {
+    app.configure(auth(app.get('auth')));
+    app.configure(local());
+    app.configure(jwt());
+    app.configure(cookies);
+  }
+
   app.use((req, res, next) => {
-    const opts = {};
-    opts.socketio = app.get('socketio');
-    opts.rest = app.get('rest');
-    opts.auth = true;
+    debug('exposing feathers utilities');
+    const opts = {
+      socketio: app.get('socketio'),
+      rest: app.get('rest'),
+      auth: app.get('auth'),
+      userAgent: req.headers['user-agent'] || ''
+    };
     Object.assign(req, { _opts: opts });
     next();
   });
 
-  app.configure(auth(app.get('auth')))
-    .configure(local())
-    .configure(jwt())
-    .configure(cookies);
-
-  app.view = {
-    render: (req, res, path, opts) => {
-      if (!res.forceCORS) {
-        res.removeHeader('Access-Control-Allow-Origin');
+  if (app.get('next')) {
+    const next = require('next');
+    debug('setting up next.js');
+    app.nextServer = next({
+      dir: app.get('next'),
+      dev: !isProduction,
+      quiet: !isProduction
+    });
+    app.view = {
+      render: (req, res, path, opts) => {
+        if (!res.forceCORS) {
+          res.removeHeader('Access-Control-Allow-Origin');
+        }
+        app.nextServer.render(req, res, path, opts);
       }
-      view.render(req, res, path, opts);
-    }
-  };
+    };
+  }
 
   return app;
 };
 
-exports.run = function () {
-  const handle = view.getRequestHandler();
-  app.get('*', (req, res) => {
-    res.removeHeader('Access-Control-Allow-Origin');
-    return handle(req, res);
-  });
-  app.configure(catcher);
-  view.prepare().then(() => {
-    const port = app.get('port');
-    app.listen(port, (err) => {
-      if (err) throw err;
-      console.log(`> Ready on http://localhost:${port}`);
+const run = () => {
+  if (app.get('next')) {
+    const handle = app.nextServer.getRequestHandler();
+
+    app.get('*', (req, res) => {
+      res.removeHeader('Access-Control-Allow-Origin');
+      return handle(req, res);
     });
+    app.configure(catcher);
+
+    if (isProduction) {
+      debug('building nextjs production bundle');
+      const builder = require('next/dist/server/build').default;
+      builder(app.get('next')).then(() => server());
+    } else {
+      debug('preparing nextjs dev server');
+      app.nextServer.prepare().then(() => server());
+    }
+  } else {
+    app.configure(catcher);
+    server();
+  }
+};
+
+const server = () => {
+  debug('starting server');
+  const port = app.get('port');
+
+  app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
   });
 };
+
+module.exports.init = init;
+module.exports.run = run;
