f1b55e6cb6906878ff59451e7bc26c828c90c690
Félix-Antoine Paradis <felix@ozyx.net>
2017-04-27 09:52:43 -0400

    feat(view): adding middleware to fetch data

--- src/view/index.js
+++ src/view/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const next = require('next');
-
+const url = require('url');
+const { app } = henri;
 const dev = process.env.NODE_ENV !== 'production';
 
 console.log(path.resolve('./app/views'));
@@ -12,5 +13,20 @@ const reactView = next({
 if (!global['henri']) {
   global['henri'] = {};
 }
+
+app.use((req, res, next) => {
+  res.locals._req = req;
+  res.render = (route, data) => {
+    const opts = {
+      data,
+      query: req.query
+    };
+    if (req.url.startsWith('/_data/')) {
+      return res.json(data);
+    }
+    reactView.render(req, res, route, opts);
+  };
+  next();
+});
 
 global['henri'].next = reactView;
