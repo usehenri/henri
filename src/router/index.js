0f95d921940b0ef94affbfa50d30f0248e6798f6
Félix-Antoine Paradis <felix@ozyx.net>
2017-04-27 09:51:27 -0400

    exposing routes in dev

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,20 @@ const path = require('path');
 
 const { app, controllers, log, next } = henri;
 
+henri._globalRoutes = [];
 const routes = require(path.resolve('./app/routes'));
+
+function register(verb, route, controller, fn) {
+  if (typeof fn === 'function') {
+    app[verb](route, fn);
+  }
+  const name = `${verb} ${route}`;
+  const entry = {};
+  entry[
+    name
+  ] = `${controller}${typeof fn !== 'function' ? ' (unknown controller)' : ' (ok)'}`;
+  henri._globalRoutes.push(entry);
+}
 
 for (const key in routes) {
   const controller = routes[key];
@@ -11,14 +24,18 @@ for (const key in routes) {
   let route = routeKey.length > 1 ? routeKey[1] : key;
 
   if (controllers.hasOwnProperty(controller)) {
-    app[verb](route, controllers[controller]);
-    app[verb](`/_data${route}`, controllers[controller]);
+    register(verb, route, controller, controllers[controller]);
+    register(verb, `/_data${route}`, controller, controllers[controller]);
     log.info(`${key} => ${controller}: registered.`);
   } else {
+    register(verb, route, controller);
     log.error(`${key} => ${controller}: unknown controller for route `);
   }
 }
 
+if (process.env.NODE_ENV !== 'production') {
+  app.get('/_routes', (req, res) => res.json(henri._globalRoutes));
+}
 next.prepare().then(() => {
   const handle = next.getRequestHandler();
   henri.app.get('*', (req, res) => {
