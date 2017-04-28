aac168e0c919e96c157bf9c48b653536a54d0936
Félix-Antoine Paradis <felix@ozyx.net>
2017-04-28 09:26:22 -0400

    fix(view): resolve next module in current directory

--- src/view/index.js
+++ src/view/index.js
@@ -1,10 +1,9 @@
 const path = require('path');
-const next = require('next');
+const next = require(path.resolve(process.cwd(), 'node_modules', 'next'));
 const url = require('url');
 const { app } = henri;
 const dev = process.env.NODE_ENV !== 'production';
 
-console.log(path.resolve('./app/views'));
 const reactView = next({
   dir: path.resolve('./app/views'),
   dev
