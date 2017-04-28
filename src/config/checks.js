eb98dee321bd1e939965c0a0b658e9c8471bcebd
Félix-Antoine Paradis <felix@ozyx.net>
2017-04-28 11:06:52 -0400

    feat(config): check for react and react-dom also

--- src/config/checks.js
+++ src/config/checks.js
@@ -1,4 +1,5 @@
 const spawn = require('child_process').spawnSync;
+const path = require('path');
 
 const yarnExists = spawn('yarn', ['help']);
 
@@ -12,6 +13,22 @@ try {
     Try installing it:
 
     # ${yarnExists ? 'yarn add' : 'npm install'} next
+    `
+  );
+  process.exit(-1);
+}
+
+try {
+  require.resolve(path.resolve(process.cwd(), 'node_modules', 'react'));
+  require.resolve(path.resolve(process.cwd(), 'node_modules', 'react-dom'));
+} catch (e) {
+  console.log(
+    `
+    Unable to load 'react' or 'react-dom' from the current project.
+
+    Try installing it:
+
+    # ${yarnExists ? 'yarn add' : 'npm install'} react react-dom
     `
   );
   process.exit(-1);
