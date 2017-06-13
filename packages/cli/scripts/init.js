990cba796a71b061003a66018b166dc1f687fed6
Félix-Antoine Paradis <felix@ozyx.net>
2017-06-13 09:03:57 -0400

    [cli] upgrade next.js

--- packages/cli/scripts/init.js
+++ packages/cli/scripts/init.js
@@ -27,7 +27,7 @@ const main = (args, name) => {
   pkg.dependencies = pkg.dependencies || {};
 
   // Add required dependencies
-  pkg.dependencies['next'] = pkg.dependencies['next'] || '^3.0.0-beta9';
+  pkg.dependencies['next'] = pkg.dependencies['next'] || '^3.0.0-beta14';
   pkg.dependencies['react'] = pkg.dependencies['react'] || '^15.5.4';
   pkg.dependencies['react-dom'] = pkg.dependencies['react-dom'] || '^15.5.4';
 
