1f86631ee76fb987e94590558ada81691cd24098
Félix-Antoine Paradis <felixp@gmail.com>
2017-11-17 16:12:33 -0500

    feat(user): add roles support (mongoose)
    On any given user object, you can use:
    `await user.hasRole('someRole');`
    or a combination that should match all:
    `await user.hasRole(['someRole', 'admin']);`
    
    It returns `true` or `false`
    
    Also, you can configure the base role for new users with `baseRole: 'guest'`

--- packages/mongoose/index.js
+++ packages/mongoose/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { log } = henri;
+const { config, log } = henri;
 
 if (global.Promise) {
   mongoose.Promise = global.Promise;
@@ -23,11 +23,18 @@ class Mongoose {
       log.info(`Found a user model (${model.globalId}), overloading it.`);
       schema.add({ email: { type: String, required: true } });
       schema.add({ password: { type: String, required: true } });
+      const baseRole =
+        (config.has('baseRole') && [config.get('baseRole')]) || [];
+      schema.add({ roles: { type: Array, default: baseRole } });
       schema.pre('save', async function(next) {
         if (!this.isModified('password')) return next();
         this.password = await user.encrypt(this.password);
         next();
       });
+      schema.methods.hasRole = async function(roles = []) {
+        let given = Array.isArray(roles) ? roles : [roles];
+        return given.every(element => this.roles.includes(element));
+      };
       isUser = true;
     }
     const instance = this.mongoose.model(model.globalId, schema);
