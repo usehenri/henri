9953147ac23341e4ef7e1d883dc37bc74a92c069
Félix-Antoine Paradis <felixp@gmail.com>
2017-11-23 10:27:05 -0500

    feat(react): adding withHenri HOC and forms components (WIP)

---
+++ packages/react/taskfile.js
@@ -0,0 +1,19 @@
+export async function compile(task) {
+  await task.parallel(['lib']);
+}
+
+export async function lib(task, opts) {
+  await task
+    .source(opts.src || 'lib/**/*.js')
+    .babel()
+    .target('dist/lib');
+}
+
+export async function release(task, opts) {
+  await task.clear('dist').start('lib');
+}
+
+export default async function(task) {
+  await task.start('lib');
+  await task.watch('lib/**/*.js', 'lib');
+}
