4057150ce1daaf2d90f905545db39cf53000c9a6
Félix-Antoine Paradis <felixp@gmail.com>
2017-12-07 13:09:00 -0500

    feat(disk): using sequelize with sqlite

--- packages/disk/index.js
+++ packages/disk/index.js
@@ -1,1 +1,17 @@
-module.exports = require('sails-disk');
+const Sql = require('@usehenri/sequelize');
+
+const { cwd } = henri;
+
+class Disk extends Sql {
+  constructor(name, config) {
+    super(name, config);
+    this.connector = new this.Sequelize(
+      `sqlite:${cwd}/.tmp/${this.name}.disk.db`,
+      {
+        dialectModulePath: require.resolve('sqlite3'),
+      }
+    );
+  }
+}
+
+module.exports = Disk;
