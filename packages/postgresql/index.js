0538271b0bea20334fbffa6191c29c7f6c46b395
Félix-Antoine Paradis <felixp@gmail.com>
2017-12-07 13:10:16 -0500

    feat(postgresql): using sequelize with pg

--- packages/postgresql/index.js
+++ packages/postgresql/index.js
@@ -1,1 +1,17 @@
-module.exports = require('sails-postgresql');
+const Sql = require('@usehenri/sequelize');
+
+const { log } = henri;
+
+class Postgresql extends Sql {
+  constructor(name, config) {
+    super(name, config);
+    if (!config.url) {
+      log.fatalError(`Missing url or host in store ${name}`);
+    }
+    this.connector = new this.Sequelize(config.url, {
+      dialectModulePath: require.resolve('pg'),
+    });
+  }
+}
+
+module.exports = Postgresql;
