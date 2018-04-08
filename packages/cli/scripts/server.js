14e7b56d7cf237456748e9e746ce783ed9fa8bee
Félix-Antoine Paradis <felix@ozyx.net>
2018-04-08 19:47:58 -0400

    fix(cli): add back the console only startup mode

--- packages/cli/scripts/server.js
+++ packages/cli/scripts/server.js
@@ -1,9 +1,15 @@
-const main = ({ skipView }, cb) => {
+const main = ({ consoleOnly = false }, cb) => {
   process.env.SUPPRESS_NO_CONFIG_WARNING = 'true';
+  if (consoleOnly) {
+    process.env.CONSOLE_ONLY = 'true';
+  }
 
   async function init() {
     try {
       await require('@usehenri/core');
+      if (typeof cb === 'function') {
+        cb();
+      }
     } catch (error) {
       // eslint-disable-next-line no-console
       console.dir(error, { colors: true });
