de98e4297c0fde4e903c08f0e55c8c275008ccde
Félix-Antoine Paradis <felix@ozyx.net>
2018-04-08 19:49:59 -0400

    feat(disk): changing back to waterline and sails-disk

--- packages/disk/index.js
+++ packages/disk/index.js
@@ -1,16 +1,105 @@
-const Sql = require('@usehenri/sequelize');
+const Waterline = require('waterline');
+const disk = require('sails-disk');
 
-const { cwd } = henri;
+const { pen } = henri;
 
-class Disk extends Sql {
+class Disk {
   constructor(name, config) {
-    super(name, config);
-    this.connector = new this.Sequelize(
-      `sqlite:${cwd}/.tmp/${this.name}.disk.db`,
-      {
-        dialectModulePath: require.resolve('sqlite3'),
+    this.name = name;
+    this.config = config;
+    this.models = {};
+    this.user = null;
+    this.waterline = new Waterline();
+    this.instance = null;
+  }
+
+  addModel(model, user) {
+    let obj = Object.assign({}, model);
+    if (obj.identity === user) {
+      obj = this.overload(obj, user);
+      this.user = obj.globalId;
+    }
+
+    obj.schema._id = {
+      type: 'number',
+      autoMigrations: { autoIncrement: true },
+    };
+    obj.primaryKey = '_id';
+
+    obj.attributes = model.schema;
+    obj.datastore = this.name;
+
+    delete obj.schema;
+    delete obj.graphql;
+
+    const instance = Waterline.Model.extend(obj);
+    this.waterline.registerModel(instance);
+    this.models[obj.globalId] = instance;
+    return this.models[obj.globalId];
+  }
+
+  overload(model, user) {
+    pen.info('disk', `user model`, model.globalId, `overloading...`);
+
+    model.schema.email = { type: 'string', required: true };
+    model.schema.password = { type: 'string', required: true };
+
+    model.beforeCreate = async (values, cb) => {
+      values.password = await user.encrypt(values.password);
+      cb();
+    };
+    model.beforeUpdate = async (values, cb) => {
+      if (values.hasOwnProperty('password')) {
+        values.password = await user.encrypt(values.password);
       }
-    );
+      cb();
+    };
+    model.hasRole = async function(roles = []) {
+      let given = Array.isArray(roles) ? roles : [roles];
+      return given.every(element => this.roles.includes(element));
+    };
+  }
+
+  getModels() {
+    return this.models || {};
+  }
+
+  async start() {
+    return new Promise((resolve, reject) => {
+      var config = {
+        adapters: {
+          disk: disk,
+        },
+
+        datastores: {
+          [this.name]: {
+            adapter: 'disk',
+          },
+        },
+      };
+      this.waterline.initialize(config, (err, orm) => {
+        if (err) {
+          pen.fatal('disk', err);
+        }
+        this.instance = orm;
+        for (let name in this.models) {
+          global[name] = orm.collections[name.toLowerCase()];
+        }
+        resolve();
+      });
+    });
+  }
+
+  async stop() {
+    return new Promise((resolve, reject) => {
+      this.waterline.teardown(err => {
+        if (err) {
+          pen.error('disk', 'something went wrong while stopping the orm', err);
+          return resolve(err);
+        }
+        setTimeout(() => resolve(), 250);
+      });
+    });
   }
 }
 
