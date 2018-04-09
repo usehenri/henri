b9f837131924d16109894ea003dd28e709cd6266
Félix-Antoine Paradis <felix@ozyx.net>
2018-04-08 21:01:51 -0400

    test(core): beginning the models tests

--- packages/core/src/__tests__/models.spec.js
+++ packages/core/src/__tests__/models.spec.js
@@ -1,9 +1,12 @@
 const BaseModule = require('../base/module');
 const Henri = require('../henri');
+const Model = require('../2.model');
 
 describe('models', () => {
   beforeAll(async () => {
-    this.henri = new Henri({ runlevel: 2 });
+    this.henri = new Henri({
+      runlevel: 2,
+    });
     await this.henri.init();
   });
 
@@ -14,4 +17,32 @@ describe('models', () => {
   test('should extend BaseModule', () => {
     expect(this.henri.model).toBeInstanceOf(BaseModule);
   });
+
+  test('should match snapshot', () => {
+    const model = new Model();
+    expect(model).toMatchSnapshot();
+  });
+
+  test('should load models', () => {
+    this.henri.pen.fatal = jest.fn();
+
+    expect(this.henri.model.ids).toEqual([]);
+    expect(this.henri.model.models).toEqual([]);
+    expect(this.henri.model.stores).toEqual({});
+
+    this.henri.model.configure(models);
+
+    expect(this.henri.pen.fatal).toHaveBeenCalledTimes(1);
+  });
 });
+
+const models = {
+  artwork: {
+    schema: {
+      title: String,
+      year: Number,
+    },
+    identity: 'artwork',
+    globalId: 'Artwork',
+  },
+};
