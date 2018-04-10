4eaae0f66b68340ce81b7854a21ed2882cbdc0e4
Félix-Antoine Paradis <felix@ozyx.net>
2018-04-10 13:47:21 -0400

    fix(cli): better packages usage

--- packages/cli/template/default/app/models/Tasks.js
+++ packages/cli/template/default/app/models/Tasks.js
@@ -1,7 +1,7 @@
 module.exports = {
   identity: 'tasks',
   datastore: 'default', // see the demo configuration up there
-  attributes: {
+  schema: {
     name: { type: 'string', required: true },
     sex: { type: 'string', required: false },
     category: {
