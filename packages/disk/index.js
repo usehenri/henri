9f23fc823dac1c6d77c602512610f019234dc83c
Félix-Antoine Paradis <felix@ozyx.net>
2018-04-15 15:10:09 -0400

    test: better test and doc

--- packages/disk/index.js
+++ packages/disk/index.js
@@ -1,7 +1,20 @@
 const Waterline = require('waterline');
 const disk = require('sails-disk');
 
+/**
+ * Disk database adapter
+ *
+ * @class Disk
+ */
 class Disk {
+  /**
+   * Creates an instance of Disk.
+   *
+   * @param {string} name Store name
+   * @param {any} config Store configuration
+   * @param {Henri} henri Current henri instance
+   * @memberof Disk
+   */
   constructor(name, config, henri) {
     this.adapterName = 'disk';
     this.name = name;
@@ -10,9 +23,25 @@ class Disk {
     this.user = null;
     this.waterline = new Waterline();
     this.instance = null;
+    this.sessionPath = '.tmp/nedb-sessions.db';
     this.henri = henri;
+
+    this.addModel = this.addModel.bind(this);
+    this.overload = this.overload.bind(this);
+    this.getModels = this.getModels.bind(this);
+    this.getSessionConnector = this.getSessionConnector.bind(this);
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
   }
 
+  /**
+   * Add a model to the store
+   *
+   * @param {object} model The model object
+   * @param {string} user The user object name
+   * @returns {object} The model instance (initialized)
+   * @memberof Disk
+   */
   addModel(model, user) {
     let obj = Object.assign({}, model);
     let isUser = false;
@@ -39,26 +68,33 @@ class Disk {
 
     this.waterline.registerModel(instance);
     if (isUser) {
-      henri._user = instance;
+      this.user = obj.globalId;
     }
     this.models[obj.globalId] = instance;
 
     return this.models[obj.globalId];
   }
 
-  overload(model, user) {
-    this.pen.info('disk', `user model`, model.globalId, `overloading...`);
+  /**
+   * Overload the user entity
+   *
+   * @param {any} model Current model
+   * @returns {object} the model
+   * @memberof Disk
+   */
+  overload(model) {
+    this.henri.pen.info('disk', `user model`, model.globalId, `overloading...`);
 
     model.schema.email = { required: true, type: 'string' };
     model.schema.password = { required: true, type: 'string' };
 
     model.beforeCreate = async (values, cb) => {
-      values.password = await user.encrypt(values.password);
+      values.password = await this.henri.user.encrypt(values.password);
       cb();
     };
     model.beforeUpdate = async (values, cb) => {
       if (values.hasOwnProperty('password')) {
-        values.password = await user.encrypt(values.password);
+        values.password = await this.henri.user.encrypt(values.password);
       }
       cb();
     };
@@ -67,21 +103,42 @@ class Disk {
 
       return given.every(element => this.roles.includes(element));
     };
+
+    return model;
   }
 
+  /**
+   * Returns the models of this store
+   *
+   * @returns {object} the models
+   * @memberof Disk
+   */
   getModels() {
     return this.models || {};
   }
 
+  /**
+   * Returns the session connector (for connect styles session storage)
+   *
+   * @param {function} session session-store function
+   * @returns {object} a store
+   * @memberof Disk
+   */
   getSessionConnector(session) {
     // eslint-disable-next-line global-require
     const NedbStore = require('nedb-session-store')(session);
 
     return new NedbStore({
-      filename: '.tmp/nedb-sessions.db',
+      filename: this.sessionPath,
     });
   }
 
+  /**
+   * Start the store
+   *
+   * @returns {Promise} Resolves or not
+   * @memberof Disk
+   */
   async start() {
     return new Promise(resolve => {
       var config = {
@@ -98,11 +155,14 @@ class Disk {
 
       this.waterline.initialize(config, (err, orm) => {
         if (err) {
-          this.henri.pen.fatal('disk', err);
+          throw err;
         }
         this.instance = orm;
         for (let name in this.models) {
           if (typeof this.models[name] !== 'undefined') {
+            if (name === this.user) {
+              this.henri._user = orm.collections[name.toLowerCase()];
+            }
             global[name] = orm.collections[name.toLowerCase()];
           }
         }
@@ -111,6 +171,12 @@ class Disk {
     });
   }
 
+  /**
+   * Stops the store
+   *
+   * @returns {Promise} Success or not?
+   * @memberof Disk
+   */
   async stop() {
     return new Promise(resolve => {
       this.waterline.teardown(err => {
--- packages/mongoose/index.js
+++ packages/mongoose/index.js
@@ -5,8 +5,20 @@ if (global.Promise) {
   mongoose.Promise = global.Promise;
 }
 
+/**
+ * Mongoose database adapter
+ *
+ * @class Mongoose
+ */
 class Mongoose {
-  constructor(name, config) {
+  /**
+   * Creates an instance of Mongoose.
+   * @param {string} name Store name
+   * @param {any} config Store configuration
+   * @param {Henri} henri Current henri instance
+   * @memberof Mongoose
+   */
+  constructor(name, config, henri) {
     if (!config.url && !config.host) {
       pen.fatal('mongoose', `Missing url or host in store ${name}`);
     }
@@ -15,10 +27,24 @@ class Mongoose {
     this.config = config;
     this.models = {};
     this.mongoose = mongoose;
+    this.henri = henri;
 
+    this.addModel = this.addModel.bind(this);
+    this.overload = this.overload.bind(this);
+    this.getModels = this.getModels.bind(this);
     this.getSessionConnector = this.getSessionConnector.bind(this);
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
   }
 
+  /**
+   * Add a model to the store
+   *
+   * @param {object} model The model object
+   * @param {string} user The user object name
+   * @returns {object} The model instance (initialized)
+   * @memberof Mongoose
+   */
   addModel(model, user) {
     let isUser = false;
 
@@ -36,15 +62,28 @@ class Mongoose {
     }
 
     this.models[model.globalId] = instance;
+
     return this.models[model.globalId];
   }
 
+  /**
+   * Overload the user entity
+   *
+   * @param {any} schema The schema
+   * @param {any} model  The model
+   * @param {any} user The user name
+   * @returns {object} The model
+   * @memberof Mongoose
+   */
   overload(schema, model, user) {
+    const { pen } = this.henri;
+
     pen.info('mongoose', `user model`, model.globalId, `overloading...`);
-    schema.add({ email: { type: String, required: true } });
-    schema.add({ password: { type: String, required: true } });
+    schema.add({ email: { required: true, type: String } });
+    schema.add({ password: { required: true, type: String } });
     const baseRole = (config.has('baseRole') && [config.get('baseRole')]) || [];
-    schema.add({ roles: { type: Array, default: baseRole } });
+
+    schema.add({ roles: { default: baseRole, type: Array } });
     schema.pre('save', async function(next) {
       if (!this.isModified('password')) return next();
       this.password = await user.encrypt(this.password);
@@ -52,23 +91,44 @@ class Mongoose {
     });
     schema.methods.hasRole = async function(roles = []) {
       let given = Array.isArray(roles) ? roles : [roles];
+
       return given.every(element => this.roles.includes(element));
     };
   }
 
+  /**
+   * Returns the models of this store
+   *
+   * @returns {object} the models
+   * @memberof Mongoose
+   */
   getModels() {
     return this.mongoose.models || {};
   }
 
+  /**
+   * Returns the session connector (for connect styles session storage)
+   *
+   * @param {function} session session-store function
+   * @returns {object} a store
+   * @memberof Mongoose
+   */
   getSessionConnector(session) {
+    // eslint-disable-next-line global-require
     const MongoStore = require('connect-mongo')(session);
 
     return new MongoStore({
-      mongooseConnection: this.mongoose.connection,
       collection: 'henriSessions',
+      mongooseConnection: this.mongoose.connection,
     });
   }
 
+  /**
+   * Start the store
+   *
+   * @returns {Promise} Resolves or not
+   * @memberof Mongoose
+   */
   async start() {
     return new Promise((resolve, reject) => {
       mongoose
@@ -77,6 +137,12 @@ class Mongoose {
     });
   }
 
+  /**
+   * Stops the store
+   *
+   * @returns {Promise} Success or not?
+   * @memberof Mongoose
+   */
   async stop() {
     return new Promise((resolve, reject) => {
       mongoose.disconnect().then(() => resolve(), err => reject(err));
