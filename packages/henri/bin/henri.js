8d13fa0790d2743f552c31d822dfd16af01ebd00
Felix-Antoine Paradis <felixp@gmail.com>
2019-04-12 16:56:26 -0400

    fix(cli): adding a check to be sure that NodeJS version is >= 10

--- packages/henri/bin/henri.js
+++ packages/henri/bin/henri.js
@@ -2,6 +2,38 @@
 
 const pkg = require('../package.json');
 
+const MINIMUM_VERSION = 10.0;
+
+const version = process.version;
+const check = parseFloat(version.substr(1, version.length)) > MINIMUM_VERSION;
+
+if (!check) {
+  const isNVM = process.env['NVM_DIR'];
+
+  console.log('');
+  console.log(
+    'You are using Node.js',
+    version,
+    isNVM !== 'undefined' ? 'with NVM' : ''
+  );
+  console.log('');
+
+  if (typeof isNVM === 'undefined') {
+    console.log('You should upgrade to a version higher than Node.js 10.x');
+    console.log('');
+    console.log('See https://nodejs.org/en/download/');
+  } else {
+    console.log(
+      'You should switch to a higher version of node using "nvm install --lts"'
+    );
+    console.log('');
+    console.log('See https://github.com/creationix/nvm');
+  }
+  console.log('');
+
+  process.exit(1);
+}
+
 try {
   // eslint-disable-next-line global-require
   require('@usehenri/cli')(pkg, process.argv);
